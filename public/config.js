aa320f1a22c4f7394f0ed07f40ac49e336e75ba7
Kdixon04 <keyandixon@gmail.com>
2025-10-23 11:11:56 -0600

    added config.js

---
+++ public/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvugQKctFfXzUZpT6CTceQupWsSTc93Zg",
+  authDomain: "itm101teamproject-2895e.firebaseapp.com",
+  projectId: "itm101teamproject-2895e",
+  storageBucket: "itm101teamproject-2895e.firebasestorage.app",
+  messagingSenderId: "543612841871",
+  appId: "1:543612841871:web:95fc7f2114dbcd750325fa",
+  measurementId: "G-TFMRG7K3DQ"
+};
